ea53a6d094bdcc8f9c80cdaf99601dc0b78ae336
WesleyMcGhee <wesmcghee6372@gmail.com>
2021-06-14 14:41:58 -0500

    exersice complete

--- calculator/calc.js
+++ calculator/calc.js
@@ -13,9 +13,22 @@ reader.question("What would you like to calculate?", function(input){
 	mathSymbol = tokens[0];
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
-
-
+	if (mathSymbol === "+"){
+		console.log(num1 + num2);
+	}else if (mathSymbol === "-"){
+		console.log(num1 - num2);
+	}else if (mathSymbol === "*"){
+		console.log(num1 * num2);
+	}else if (mathSymbol === "/"){
+		console.log(num1 / num2);
+		
+	}else if (mathSymbol === "√" || mathSymbol === "sqrt"){
+		console.log(Math.sqrt(num1));
+	}
+	
 	// This line closes the connection to the command line interface.
+	
 	reader.close()
+	
 
 });
